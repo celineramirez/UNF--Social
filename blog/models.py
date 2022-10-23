8dc5a57d41a52cee3a5581e29538bc4675d6ca38
celineramirez <crmrz28@gmail.com>
2022-10-23 17:11:31 -0400

    updated pages

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,10 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    thumbnail = models.URLField(max_length=200, blank=True)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    event_date = models.DateTimeField(blank=True, null=True)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
--- unfsocial/settings.py
+++ unfsocial/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Eastern'
 
 USE_I18N = True
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'unfsocial/blog/css/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
